fe07babd91893fb8919be07abe8bc419fba4f13f
mikuu31 <105326230+mikuu31@users.noreply.github.com>
2022-05-20 15:54:14 -0300

    Add files via upload

---
+++ ejercicio_2.py
@@ -0,0 +1,49 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica y consola
+
+# Ahora los valores a operar deben ser ingresados por
+# consola con la función "input" como se ve a continuación
+print('Ingrese por consola el primer número entero a operar:')
+numero_1 = int(input())
+
+print('Ingrese por consola el segundo número entero a operar:')
+numero_2 = int(input())
+
+# Alumno: Imprima en pantalla los dos números enteros solicitados
+# print(....)
+
+print('el numero_1 es:', numero_1)
+print('el numero_2 es:', numero_2)
+
+
+# Alumno: Calcule la suma, resta, división y multiplicación de los números ingresados
+# numero_1, numero_2
+# Imprima en pantalla todos los resultados con el siguiente formato de ejemplo:
+# El resultado de sumar 4 y 2 es 6
+# NOTA: No coloque usted los nùmeros y resultados, use las variables
+
+# Suma
+
+suma = numero_1 + numero_2
+print('la suma de numero_1 y numero_2 es:', suma)
+
+# Resta
+resta = numero_1 - numero_2 
+print('la resta de numero_1 y numero_2 es:', resta)
+
+# División
+
+division = numero_1 / numero_2 
+print('la division de numero_1 y numero_2 es:', division)
+
+# Multiplicación
+multiplicacion = numero_1 * numero_2 
+print('la multiplicacion de numero_1 y numero_2 es:', multiplicacion)
